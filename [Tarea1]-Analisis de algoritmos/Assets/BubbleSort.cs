001df4f3ee3c7395c613f5f9597f5291dd8769cf
AgusBA011 <agustinblanco01@gmail.com>
2020-02-25 23:12:02 -0600

    Última update

--- [Tarea1]-Analisis de algoritmos/Assets/BubbleSort.cs
+++ [Tarea1]-Analisis de algoritmos/Assets/BubbleSort.cs
@@ -7,28 +7,92 @@ public class BubbleSort : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    const int arraysize = 1000;
-    int[] arr = new int[arraysize];
+    int arraysize = 0;
+
+    int[] arr0 = new int[1000];
+    int[] arr1 = new int[2000];
+    int[] arr2 = new int[3000];
+    int[] arr3 = new int[4000];
+    int[] arr4 = new int[5000];
+    int[] arr5 = new int[6000];
+    int[] arr6 = new int[7000];
+    int[] arr7 = new int[8000];
+    int[] arr8 = new int[9000];
+    int[] arr9 = new int[10000];
+
+
+    int[][] MainArray = new int[10][];
+
     int k;
+    float bolaX = -2.21f;
     Stopwatch sw = new Stopwatch();
+
+    public GameObject punto;
+    public GameObject tiempo;
+    public TextMesh tm;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        MainArray[0] = arr0;
+        MainArray[1] = arr1;
+        MainArray[2] = arr2;
+        MainArray[3] = arr3;
+        MainArray[4] = arr4;
+        MainArray[5] = arr5;
+        MainArray[6] = arr6;
+        MainArray[7] = arr7;
+        MainArray[8] = arr8;
+        MainArray[9] = arr9;
+
         System.Random rnd = new System.Random();
-        for (int i = 0; i < arraysize; i++)
+
+        for (int i = 0; i < 10; i++)
         {
-            k = rnd.Next(500);
-            arr[i] = k;
+            arraysize = arraysize + 1000;
+
+            for (int j = 0; j < arraysize; j++)
+            {
+                k = rnd.Next(500);
+                MainArray[i][j] = k;
+            }
+
+            int len = MainArray[i].Length;
+
+            sw.Start();
+
+            bubbleSort(MainArray[i]);
+
+            sw.Stop();
+
+            UnityEngine.Debug.Log("(BubbleSort)Time elapsed:" + ((double)(sw.Elapsed.TotalMilliseconds * 1000000) /
+                1000000).ToString("0.00 ms"));
+
+            float timeElapsed = sw.Elapsed.Milliseconds;
+
+            UnityEngine.Debug.Log(timeElapsed);
+
+            float bolaY = -3.1f + timeElapsed;
+
+            float tiempoY = -3.1f + timeElapsed;
+
+            Instantiate(punto);
+            punto.transform.position = new Vector3(bolaX, 0.36f, bolaY);
+
+            Instantiate(tiempo);
+
+            tiempo.transform.position = new Vector3(-8.12f, 0.36f, tiempoY);
+            tiempo.transform.eulerAngles = new Vector3(90, 0, 0);
+
+            TextMesh tm = new TextMesh();
+            tm = tiempo.GetComponent("TextMesh") as TextMesh;
+            tm.text = ((double)(sw.Elapsed.TotalMilliseconds * 1000000) /
+                1000000).ToString("0.00") + "---";
+
+            bolaX = bolaX + 4f;
 
         }
-
-        sw.Start();
-        bubbleSort(arr);
-        sw.Stop();
-
-        UnityEngine.Debug.Log("(Bubble Sort)Time elapsed:" + ((double)(sw.Elapsed.TotalMilliseconds * 1000000) /
-            1000000).ToString("0.00 ms"));
 
     }
 
